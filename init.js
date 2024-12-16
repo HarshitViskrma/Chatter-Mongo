fbe3ccfd2f79b37ffc228737a8a41e09b2e7a905
Harshit Vishwakarma <130080307+HarshitViskrma@users.noreply.github.com>
2024-12-16 23:09:25 +0530

    add new file

---
+++ init.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Chat = require('./models/chat.js');
+
+
+main()
+.then(() => {
+    console.log("Database formed");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [
+    {
+        from: "neha",
+        to: "priya",
+        msg: "send me your exam sheet",
+        created_at: new Date()
+    },
+    {
+        from: "rahul",
+        to: "priya",
+        msg: "send me your exam sheet A",
+        created_at: new Date()
+    },
+    {
+        from: "priya",
+        to: "neha",
+        msg: "send exam sheet",
+        created_at: new Date()
+    },
+    {
+        from: "priya",
+        to: "rahul",
+        msg: "send exam sheet A",
+        created_at: new Date()
+    },
+];
+
+Chat.insertMany(allChats);
